bc09f05c59b78611fdfeca9a4609f0465f4836d3
prabh <prabh.dev0@gmail.com>
2026-06-06 20:02:17 +0530

    add planet class

---
+++ planet_class.py
@@ -0,0 +1,26 @@
+class Planet:
+    def __init__(self,name,planet_type,star):
+     
+        if not all(isinstance(arg,str) for arg in [name,planet_type,star]):
+            raise TypeError ("name, planet type, and star must be strings")
+        if not all([name,planet_type,star]):
+            raise ValueError ("name, planet_type, and star must be non-empty strings")
+        self.name=name
+        self.planet_type=planet_type
+        self.star=star   
+
+    def orbit(self):
+        return f"{self.name} is orbiting around {self.star}..."
+    def __str__(self):
+        return f"Planet: {self.name} | Type: {self.planet_type} | Star: {self.star}"
+planet_1 = Planet("earth","rocky","sun")
+planet_2 = Planet("jupiter","gas_giant","sun")
+planet_3 = Planet("mars","rocky","sun")
+
+print(planet_1)
+print(planet_2)
+print(planet_3)
+
+print(planet_1.orbit())
+print(planet_2.orbit())
+print(planet_3.orbit())
